f608e9f1dc7d15739b229b78441d06853ab70abf
D'Onofrio Alessandro <alessandro.donofrio.consultant@yoox.com>
2018-05-20 00:35:18 +0200

    Model has been updated and Controllers have been added.

--- MoviesAPI/Models/Rating.cs
+++ MoviesAPI/Models/Rating.cs
@@ -1,10 +1,15 @@
-﻿namespace MoviesAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesAPI.Models
 {
     public class Rating
     {
         public int Id { get; set; }
+        [Required]
+        public short Score { get; set; }
         public int UserId { get; set; }
         public int MovieId { get; set; }
-        public short Score { get; set; }
+        public Movie Movie { get; set; }
+        public User User { get; set; }
     }
 }
--- MoviesAPI/Models/User.cs
+++ MoviesAPI/Models/User.cs
@@ -1,10 +1,11 @@
-﻿namespace MoviesAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesAPI.Models
 {
     public class User
     {
         public int Id { get; set; }
+        [Required]
         public string UserName { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
     }
 }
