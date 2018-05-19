3799e6deb7a5f01cff624d25df21145f0e3b5b4a
D'Onofrio Alessandro <alessandro.donofrio.consultant@yoox.com>
2018-05-19 23:51:34 +0200

    Properties added into Movie class.

--- MoviesAPI/Models/Movie.cs
+++ MoviesAPI/Models/Movie.cs
@@ -1,6 +1,11 @@
-﻿namespace MoviesAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesAPI.Models
 {
     public class Movie
     {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
     }
 }
