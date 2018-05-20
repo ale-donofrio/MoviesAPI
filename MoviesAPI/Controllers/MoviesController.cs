85a4d945e3d178f54815847cebfddaa1a16f85fc
D'Onofrio Alessandro <alessandro.donofrio.consultant@yoox.com>
2018-05-20 19:11:57 +0200

    A char has been removed.

--- MoviesAPI/Controllers/MoviesController.cs
+++ MoviesAPI/Controllers/MoviesController.cs
@@ -9,7 +9,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
-using MoviesAPI.Models;\
+using MoviesAPI.Models;
 using MoviesAPI.Models.DTO;
 
 namespace MoviesAPI.Controllers
